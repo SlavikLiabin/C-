3a59b45d327932cc8e715f3e2ace57b77b773d39
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2024-05-29 19:35:24 +0500

    Update Program.cs
    NameDishes

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -5,48 +5,19 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+        (string Name, string[] Dishes) User;
+        Console.WriteLine("Введите имя пользователя");
+        User.Name = Console.ReadLine();
 
-        for (int k = 0; k < 3; k++)
+        User.Dishes = new string[5];
+        for(int i = 0; i < User.Dishes.Length; i++)
         {
-            Console.WriteLine("Введите имя: ");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите фамилию: ");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Введите логин: ");
-            User.Login = Console.ReadLine();
-
-            User.LoginLength = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да или нет? ");
-            var result = Console.ReadLine();
-
-            if (result == "Да")
-            {
-                User.HasPet = true;
-            }
-            else
-            {
-                User.HasPet = false;
-            }
-
-            Console.WriteLine("Введите возраст пользователя");
-
-            User.Age = double.Parse(Console.ReadLine());
-
-            User.favcolors = new string[3];
-            Console.WriteLine("Введите три любимых цвета пользователя");
-
-            for (int i = 0; i < User.favcolors.Length; i++)
-            {
-                User.favcolors[i] = Console.ReadLine();
-            }
-
-                      
+            Console.WriteLine("Введите любимое блюдо номер {0}", i + 1);
+            User.Dishes[i] = Console.ReadLine();
         }
-             
+
+        Console.ReadKey();
+                             
+        }
 
     }
-}
