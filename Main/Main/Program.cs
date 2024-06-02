c5d4d3a6f124ff9238d71621be12fd305c8632bd
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2024-06-02 22:02:38 +0500

    Update Program.cs
    favcolors(3)

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,23 +1,71 @@
 using System;
 
-class MainClass
-{
-    public static void Main(string[] args)
-    {
+class MainClass {
 
-        (string Name, string[] Dishes) User;
-        Console.WriteLine("Введите имя пользователя");
-        User.Name = Console.ReadLine();
+	static void ShowColor(string username, int userage)
+	{
+		Console.WriteLine("{0}, {1} лет, \nНапишите свой любимый цвет на английском с маленькой буквы", username, userage);
+    string color = Console.ReadLine();
 
-        User.Dishes = new string[5];
-        for(int i = 0; i < User.Dishes.Length; i++)
-        {
-            Console.WriteLine("Введите любимое блюдо номер {0}", i + 1);
-            User.Dishes[i] = Console.ReadLine();
-        }
+    switch (color) {
+    case "red":
+      Console.BackgroundColor = ConsoleColor.Red;
+      Console.ForegroundColor = ConsoleColor.Black;
 
-        Console.ReadKey();
-                             
-        }
+      Console.WriteLine("Your color is red!");
+      break;
 
+    case "green":
+      Console.BackgroundColor = ConsoleColor.Green;
+      Console.ForegroundColor = ConsoleColor.Black;
+
+      Console.WriteLine("Your color is green!");
+      break;
+    case "cyan":
+      Console.BackgroundColor = ConsoleColor.Cyan;
+      Console.ForegroundColor = ConsoleColor.Black;
+
+      Console.WriteLine("Your color is cyan!");
+      break;
+    default:
+      Console.BackgroundColor = ConsoleColor.Yellow;
+      Console.ForegroundColor = ConsoleColor.Red;
+
+      Console.WriteLine("Your color is yellow!");
+      break;
     }
+    return color;
+	}
+
+  public static void Main (string[] args) {
+		
+		var (name, age) = ("Евгения", 27);
+
+		Console.WriteLine("Мое имя: {0}", name);
+		Console.WriteLine("Мой возраст: {0}", age);
+
+		Console.Write("Введите имя: ");
+		name = Console.ReadLine();
+		Console.Write("Введите возраст с цифрами:");
+		age = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Ваше имя: {0}", name);
+		Console.WriteLine("Ваш возраст: {0}", age);
+
+		var favcolors = new string[3];
+
+    for (int i = 0; i < favcolors.Length; i++){
+      favcolors[i] = ShowColor(name, age);
+    }
+
+    static void Showcolors(string[] favcolors){
+      Console.WriteLine("Ваши любимые цвета: ");
+      foreach (var color in favcolors){
+        Console.WriteLine(color);
+      }
+    }
+
+    Console.ReadKey();
+	
+  }
+}
