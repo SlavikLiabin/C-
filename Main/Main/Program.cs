5d78587bbb2378573d00fb85b89c1521772171a1
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2024-06-09 10:01:34 +0500

    Update Program.cs
    checking

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -2,22 +2,23 @@ using System;
 					
 public class Program
 {
-	static void Main(string[] args) {
-  var num = 1;
-
-  Matreshka(num);
-
-  Console.ReadKey();
-
-}
-
-static int Matreshka(int num) {
-  num++;
-
-  return Sum(num, 3);
-}
-
-static int Sum(int num, int num2) {
-  return num + num2;
-}
+	static void Main(string[] args)
+	{
+		int money;
+		int food;
+		int foodUnitPrice = 10;
+		bool isAbleToPay;
+		
+		Console.WriteLine($"Добро пожаловать в пекарню! Сегодня еда по {foodUnitPrice} монет.");
+		Console.WriteLine("Сколько у вас золота?");
+		money = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Сколько еды вам нужно?");
+		food = Convert.ToInt32(Console.ReadLine());
+		
+		isAbleToPay = money >= food * foodUnitPrice;
+		food *= Convert.ToInt32(isAbleToPay);
+		
+		money -= food * foodUnitPrice;
+		Console.WriteLine($"У вас в сумке {food} единиц еды, и {money} монет.");
+	}
 }
