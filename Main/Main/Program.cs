f36ee1a5f074b3779f404e4d3bcd08971983c96e
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2024-06-20 21:39:38 +0500

    Update Program.cs
    abstract

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,21 +1,42 @@
 using System;
-					
-public class Program
-{
-	public static void Main(string[] args)
-	{
-		Random rand = new Random();
-		
-		int health1 = rand.Next(90, 100);
-		int damage1 = rand.Next(5, 20);
-		int armor1 = rand.Next(25, 65);
-		
-		int health2 = rand.Next(80, 150);
-		int damage2 = rand.Next(20, 40);
-		int armor2 = rand.Next(65, 100);
-		
-		Console.WriteLine($"Гладиатор 1 - {health1} здоровье, {damage1} наносимый урон, {armor1} броня");
-		Console.WriteLine($"Гладиатор 2 - {health2} здоровье, {damage2} наносимый урон, {armor2} броня");
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-	}
+namespace CSharpLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            abstract class ComputerPart 
+            {
+              public abstract void Work();
+            }
+
+            class Processor: ComputerPart 
+            {
+              public override void Work() 
+              {
+                Console.WriteLine("Processor");
+              }
+            }
+
+            class MotherBoard: ComputerPart 
+            {
+              public override void Work() 
+              {
+                Console.WriteLine("MotherBoard");
+              }
+            }
+            class GraphicCard: ComputerPart 
+            {
+              public override void Work() 
+              {
+                Console.WriteLine("GraphicCard");
+              }
+            }              
+        }
+    }
 }
