524cf559fb6355dec84f3e03a5ef46c48932af74
SlavikLiabin <122288413+SlavikLiabin@users.noreply.github.com>
2024-07-10 20:19:35 +0500

    Update Program.cs
    aboutDisk

--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -1,29 +1,53 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace learning
+namespace FileScaner
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            // получим системные диски
             DriveInfo[] drives = DriveInfo.GetDrives();
-            foreach (DriveInfo drive in drives) 
+
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives.Where(d => d.DriveType == DriveType.Fixed))
             {
-                Console.WriteLine($"Название: {drive.Name}");
-                Console.WriteLine($"Тип: {drive.DriveType}");
-                if(drive.IsReady)
-                {
-                    Console.WriteLine($"Объем: {drive.TotalSize}");
-                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
-                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
-                }
+                WriteDriveInfo( drive);
+                DirectoryInfo root = drive.RootDirectory;
+                var folders = root.GetDirectories();
+
+                WriteFolderInfo(folders);
+
+                Console.WriteLine();
                 Console.ReadKey();
+            }
+        }
+
+        public static void WriteDriveInfo(DriveInfo drive)
+        {
+            Console.WriteLine($"Название: {drive.Name}");
+            Console.WriteLine($"Тип: {drive.DriveType}");
+            if (drive.IsReady)
+            {
+                Console.WriteLine($"Объем: {drive.TotalSize}");
+                Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                Console.WriteLine($"Метка: {drive.VolumeLabel}");
+            }
+        }
+
+        public static void WriteFolderInfo(DirectoryInfo[] folders)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Папки: ");
+            Console.WriteLine();
+
+            foreach (var folder in folders) 
+            {
+                Console.WriteLine(folder.Name);
             }
         }
     }
 }
+
